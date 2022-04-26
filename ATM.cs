78f506c37c454afcbf8382ff6a9f151a01ae2c21
kir03 <pehenko.k@icloud.com>
2022-04-26 15:16:02 +0300

    Добавьте файлы проекта.

---
+++ ATM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class ATM
+    {
+        private int _cash = 10000;
+        public int Cash
+        {
+            get 
+            { 
+                return _cash; 
+            }
+            set
+            {
+                    _cash = value;
+            }
+        }
+        public ATM(int cash)
+        {
+            if (Cash+cash>0)
+            Cash = Cash+cash;
+        }
+        public void inf()
+        {
+            Console.WriteLine(Cash);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Bank // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ATM kir = new ATM(2000);
+            kir.inf();
+        }
+    }
+}
